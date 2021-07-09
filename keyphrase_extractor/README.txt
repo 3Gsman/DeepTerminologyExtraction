7915115239ba11ba276fabb05ae83985b0f9df1c
Germán García <germanpublingo@gmail.com>
2021-07-09 10:17:57 +0200

    Add files via upload

---
+++ keyphrase_extractor/README.txt
@@ -0,0 +1,39 @@
+keyphraseExtractor.py v1.0 09/07/2021
+German Garcia Garcia
+
+MSC in Artificial Intelligence
+Final Master´s project: Applying Deep Learning Techniques to Terminology Extraction in Specific Domains
+Universidad Politecnica de Madrid
+
+The following script is an automatic terminology or keyphrase extractor that given a text
+input returns the text itself with the keywords on it highlighted and a list of them,
+the model has been trained using the system provided in the following github repository.
+
+https://github.com/3Gsman/DeepTerminologyExtraction
+
+
+INSTALATION: 
+
+Python 3.8 or higher is requiered to use this program.
+
+This program uses the FLAIR library: https://github.com/flairNLP/flair, it can be download
+using pip with the following command: pip3 install flair
+The version used in this work is flair==0.8.0.post1
+
+Using this program requires 2 files:
+
+- input.txt: 	A plain text file where the user writes the input to be processed by the model
+
+- model.pt: 	The model file, due storage constrains, github do not let upload large files 
+				to the platform. You can access to this file in the following google drive link:
+				https://drive.google.com/file/d/1vHaj5d8yknMVe4zPr837ODMhuIax7gFw/view?usp=sharing
+				or contact with the author, German Garcia in the following email: gggsman@gmail.com
+				Then, move the model file to the working directory with the rest of the files.
+
+USAGE:
+
+To use this program you only need to execute the python script on the console once the installation 
+steps are completed. Write the desired input that you want to extract the keywords from, wait until 
+the execution finish, and obtain the result in the 'output.txt' file. The structure of the output file
+has 3 sections, the text input, the text input with the keywords highlighted and the list of the keywords.
+
---
+++ keyphrase_extractor/keyphrase_extractor.py
@@ -0,0 +1,69 @@
+# File: 		keyphraseExtractor.py v1.0 09/07/2021
+# Author:		German Garcia Garcia
+# Description:	The following script is an automatic terminology or keyphrase extractor that given a text
+#				input returns the text itself with the keywords on it highlighted and a list of them,
+#				the model has been trained using the system provided in the following github repository.
+# Github:		https://github.com/3Gsman/DeepTerminologyExtraction
+
+import sys
+import os
+
+try:
+	from flair.models import SequenceTagger
+	from flair.data import Sentence
+except:
+	print("ERROR: 'FLAIR' libray not found, please write the command 'pip3 install flair'. ")
+
+# Files loading
+if not os.path.isfile('model.pt'):
+	print("ERROR: 'model.pt' does not exist, please follow the instructions in README.txt to download the model.")
+	sys.exit()
+
+try:
+	with open('input.txt') as f:
+		lines = f.readlines()
+except IOError:
+	print("ERROR: 'input.txt' file does not exist. Please create the file in this directory.")
+	sys.exit()
+
+model = SequenceTagger.load('model.pt')	
+
+try:
+	sentence = Sentence(lines[0])
+except:
+	print("ERROR: 'input.txt' file empty. Please intruduce the text in 'input.txt'.")
+	sys.exit()
+
+# Tag preddiction
+model.predict(sentence)
+
+labeled = sentence.to_tagged_string()
+
+# Output processing
+wordList = labeled.split()
+
+it2 = -1
+keywords = []
+for i in range(len(wordList)):
+	if wordList[i] == "<B-KEY>":
+		temp = []
+		temp.append(wordList[i-1])
+		keywords.append(temp)
+
+	elif wordList[i] == "<I-KEY>":
+		temp.append(wordList[i-1])
+		keywords.append(temp)
+
+str_keyword = []
+for item in keywords:
+	str_keyword.append(' '.join(item))
+
+str_keyword = list(set(str_keyword))
+
+temp2 = ["ORIGINAL TEXT",lines[0],"","TEXT WITH TAGS",labeled,"","KEYWORDS"]
+
+totxt = temp2 + str_keyword
+
+with open('output.txt', 'w') as f:
+    for item in totxt:
+        f.write("%s\n" % item)
